9913c4e466f8a54966468c2d6cc5a15810fe363c
practicante3 <practicante@delphianalytics.co>
2023-01-02 19:38:27 -0500

    base: -Se creo la plantilla sobre la cual se va a trabajar

---
+++ src/Redux/globals/globals.js
@@ -0,0 +1,9 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const AuthSesion = createSlice({
+  name: "sesion",
+  initialState: {},
+  reducers: {},
+});
+
+export default AuthSesion.reducer;
